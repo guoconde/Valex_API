7680b66cf9020a9009ca1330cc38a57adea7ba58
Celso <celsolnv27@gmail.com>
2022-04-19 23:47:30 -0300

    Preparando revisão de código

--- src/services/companiesServices.ts
+++
@@ -1,7 +0,0 @@
-import * as companyRepository from "../repositories/companyRepository.js";
-
-export async function getCompany(key: string) {
-  const company = await companyRepository.findByApiKey(key);
-
-  return company;
-}
